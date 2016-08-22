15c324bea9a3f366fd3abf78d76c608313477f60
Mininzombie <devetaikin.a@fruitware.ru>
2016-08-22 11:48:31 +0300

    Add files via upload

---
+++ tree/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#data').jstree({
+
+        "plugins": ["checkbox"]
+    });
+$('#data').on("changed.jstree", function(e, data) {
+    if (data.selected.length) {
+        $(data.selected).each(function (idx) {
+            var node = data.instance.get_node(data.selected[idx]);
+            console.log('The selected node is: ' + node.text);
+        });
+
+    }
+});
+});
